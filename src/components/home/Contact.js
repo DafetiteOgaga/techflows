500be371dc45f2a4ae346c37a1e0ad0e64b5d56d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-21 11:13:08 +0100

    fixed Contact.js: fixed class syntax

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -2,18 +2,18 @@ import plug from '../../images/plug.png'
 
 function Contact () {
 	return (
-		<section class="contact_section layout_padding">
-			<div class="container ">
-				<div class="heading_container">
+		<section className="contact_section layout_padding">
+			<div className="container ">
+				<div className="heading_container">
 					<h2>
 					Contact Us
 					</h2>
 					<img src={plug} alt=""/>
 				</div>
 			</div>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
 						<form action="">
 							<div>
 								<input type="text" placeholder="Name" />
@@ -25,19 +25,19 @@ function Contact () {
 								<input type="text" placeholder="Phone Number" />
 							</div>
 							<div>
-								<input type="text" class="message-box" placeholder="Message" />
+								<input type="text" className="message-box" placeholder="Message" />
 							</div>
-							<div class="d-flex ">
+							<div className="d-flex ">
 								<button>
 									SEND
 								</button>
 							</div>
 						</form>
 					</div>
-					<div class="col-md-6">
-						<div class="map_container">
-							<div class="map-responsive">
-								<iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="300" frameborder="0" style={{border:0, width: '100%', height:'100%'}} title="location map" allowfullscreen></iframe>
+					<div className="col-md-6">
+						<div className="map_container">
+							<div className="map-responsive">
+								<iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="300" frameBorder="0" style={{border:0, width: '100%', height:'100%'}} title="location map" allowFullScreen></iframe>
 							</div>
 						</div>
 					</div>
