d58d4eefbedab885c814cb58275008145b1c2cba
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-21 11:14:25 +0100

    fixed Slider.js: fixed class syntax

--- src/components/home/Slider.js
+++ src/components/home/Slider.js
@@ -2,11 +2,11 @@ import sliderImages from '../../images/slider-img.jpg'
 
 function Slider () {
 	return (
-		<section class=" slider_section ">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 ">
-						<div class="detail_box">
+		<section className=" slider_section ">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6 ">
+						<div className="detail_box">
 							<h1>
 								Your Trusted <br/>
 								IT Solution <br/>
@@ -15,39 +15,39 @@ function Slider () {
 							<p>
 								It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem
 							</p>
-							<a href="/" class="">
+							<a href="/" className="">
 								Contact Us
 							</a>
 						</div>
 					</div>
-					<div class="col-lg-5 col-md-6 offset-lg-1">
-						<div class="img_content">
-							<div class="img_container">
-								<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-									<div class="carousel-inner">
-										<div class="carousel-item active">
-											<div class="img-box">
+					<div className="col-lg-5 col-md-6 offset-lg-1">
+						<div className="img_content">
+							<div className="img_container">
+								<div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+									<div className="carousel-inner">
+										<div className="carousel-item active">
+											<div className="img-box">
 												<img src={sliderImages} alt="slider img"/>
 											</div>
 										</div>
-										<div class="carousel-item">
-											<div class="img-box">
+										<div className="carousel-item">
+											<div className="img-box">
 												<img src={sliderImages} alt="slider img"/>
 											</div>
 										</div>
-										<div class="carousel-item">
-											<div class="img-box">
+										<div className="carousel-item">
+											<div className="img-box">
 												<img src={sliderImages} alt="slider img"/>
 											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-							<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-								<span class="sr-only">Previous</span>
+							<a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+								<span className="sr-only">Previous</span>
 							</a>
-							<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-								<span class="sr-only">Next</span>
+							<a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+								<span className="sr-only">Next</span>
 							</a>
 						</div>
 
