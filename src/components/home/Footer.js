6abe4096a1a7582f6235196edf8984a16c6f8fc7
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-21 11:13:20 +0100

    fixed Footer.js: fixed class syntax

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -4,10 +4,10 @@ import dafetite from '../../images/dafelogoBlackTransparent.png'
 function Footer () {
 	const year = DateHook().todayYear
 	return (
-		<footer class="container-fluid footer_section">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-7 col-md-9 mx-auto">
+		<footer className="container-fluid footer_section">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-7 col-md-9 mx-auto">
 						<p>
 							&copy; {year} All Rights Reserved | Developed by:
 							<a href="https://dafetiteogaga.github.io/dafetite/"><img src={dafetite} alt="developer logo" style={styles.dafetite} /></a>
